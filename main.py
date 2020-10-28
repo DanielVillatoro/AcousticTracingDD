56b21fc4e4448cdc95d5a164d31f6d20bfa86e7e
danielcdz <calde0601@gmail.com>
2020-10-27 23:13:12 -0600

    Agregando radar

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+# Proyecto 2 Analisis de algoritmos
+# Daniel Calderon Diaz
+# Daniel Villatoro Cantarero
 
-# Pruebas de PIL
 from PIL import Image
 import numpy as np
 import pygame
 
 
+#Imagenes del emulador
 imagen1 = np.array(Image.open('imagenprueba.jpg'))
 imagen2 = np.array(Image.open('imagenprueba.jpg'))
+
 
 
 def modificarPixeles():
     for i in range(500):
         for j in range(500):
-            if(np.array_equiv(imagen1[i,j],255)):
-                imagen1[i,j] = 5
+            if not(np.array_equiv(imagen2[i,j],0)):
+                imagen2[i,j] = 88
 
 
-# UI
-h,w=1200,800
+
+
+# --------------------------------------- UI ------------------------------------------------U
+h,w=1500,800
 border=50
 pygame.init()
 screen = pygame.display.set_mode((h, w))
@@ -34,42 +40,72 @@ font = pygame.font.Font('freesansbold.ttf', 21)
 texto1 = font.render('Imagen a escanear', True, black)
 texto2 = font.render('Imagen en proceso', True, black)
 
-
 # create a rectangular object for the
 # text surface object
 textRect1 = texto1.get_rect()
 textRect2 = texto2.get_rect()
 
-
 # set the center of the rectangular object.
 textRect1.center = (350 , 50)
-textRect2.center = (880 , 50)
+textRect2.center = (1200 , 50)
+
+
+
+# movimiento del radar
+x1 = 240
+y1 = 240
+
+x2 = 250
+y2 = 235
+
+width = 20
+height = 20
+
+
+
+vel = 10
 
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     done = True
+                if event.type == pygame.MOUSEBUTTONUP:
+                    x1, y1 = pygame.mouse.get_pos()
+                    print("primero: ",x1, y1)
+                    if(x1>950):
+                        x1-=950
+                    elif(x1<950):
+                        x1 = 950 - x1
 
-        # Clear screen to white before drawing
+                    y1-=100
+                    x2=x1+10
+                    y2=y1-5
+                    print(x1, y1)
+
+
+
         screen.fill((255, 255, 255))
-
-        # Get a numpy array to display from the simulation
-
         screen.blit(texto1,textRect1)
         screen.blit(texto2,textRect2)
 
         modificarPixeles()
-        npimage = imagen1
-        imagenE = imagen2
+        imagen1
+        imagen2
         # Convert to a surface and splat onto screen offset by border width and height
-        surface = pygame.surfarray.make_surface(npimage)
-        screen.blit(surface, (620, 100))
+        surface1 = pygame.surfarray.make_surface(imagen1)
+        screen.blit(surface1, (100, 100))
 
-        surface = pygame.surfarray.make_surface(imagenE)
-        screen.blit(surface, (100, 100))
+
+        surface2 = pygame.surfarray.make_surface(imagen2)
+
+        # X  Y W H
+        pygame.draw.rect(surface2, (255, 0, 0), (x1, y1, 80, 5))
+        pygame.draw.rect(surface2, (255, 255, 255), (x2, y2, 60, 5))
+
+        screen.blit(surface2, (950, 100))
 
         pygame.display.flip()
-        clock.tick(60)
+        pygame.display.update()
 
 
 
