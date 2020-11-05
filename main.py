4d67d6b89e23ea43338aa997c3a3bf1593abad96
danielcdz <calde0601@gmail.com>
2020-11-04 23:20:02 -0600

    Area de sonar

--- main.py
+++ main.py
@@ -19,6 +19,29 @@ rangoCono = 40
 # def enviarRayosCono():
 
 
+
+def guardarAreaSonar(x,y):
+    rangoX, rangoY  = 30,30
+    x -= rangoX
+    y -= 15
+    if (x < rangoX):
+        x = 0
+    if (y < rangoX):
+        y = 0
+    area = []
+    print(area)
+    while(rangoY != 0):
+        area += [(x,y)]
+        rangoX-=1
+        x+=1
+        if(rangoX == 0):
+            rangoX = 30
+            rangoY -= 1
+            y += 1
+    print("area sonar: ",area)
+
+
+
 def calcularXY(x,y):
     if (x > 950):
         x -= 950
@@ -31,80 +54,81 @@ def calcularXY(x,y):
 
 def calcularRayo(x,y,angulo):
     x,y = calcularXY(x,y)
+    guardarAreaSonar(x,y)
     if angulo == 0:
         enviarRayo(x,y,0,1)
     elif angulo == 10:
         enviarRayo(x, y, 1, 2)
-    elif angulo == -10:
+    elif angulo == 350:
         enviarRayo(x, y, -1, 2)
     elif angulo == 20:
         enviarRayo(x, y, 2, 2)
-    elif angulo == -20:
+    elif angulo == 340:
         enviarRayo(x, y, -2, 2)
     elif angulo == 30:
         enviarRayo(x, y, 3, 3)
-    elif angulo == -30:
+    elif angulo == 330:
         enviarRayo(x, y, -3, 3)
     elif angulo == 40:
         enviarRayo(x, y, 4, 3)
-    elif angulo == -40:
+    elif angulo == 320:
         enviarRayo(x, y, -4, 3)
     elif angulo == 50:
         enviarRayo(x, y, 5, 4)
-    elif angulo == -50:
+    elif angulo == 310:
         enviarRayo(x, y, -5, 4)
     elif angulo == 60:
         enviarRayo(x, y, 6, 4)
-    elif angulo == -60:
+    elif angulo == 300:
         enviarRayo(x, y, -6, 4)
     elif angulo == 70:
         enviarRayo(x, y, 7, 5)
-    elif angulo == -70:
+    elif angulo == 290:
         enviarRayo(x, y, -7, 5)
     elif angulo == 80:
         enviarRayo(x, y, 8, 5)
-    elif angulo == -80:
+    elif angulo == 280:
         enviarRayo(x, y, -8, 5)
     elif angulo == 90:
         enviarRayo(x, y, 1, 0)
-    elif angulo == -90:
+    elif angulo == 270:
         enviarRayo(x, y, -1, 0)
     elif angulo == 100:
         enviarRayo(x, y, 2, -1)
-    elif angulo == -100:
+    elif angulo == 260:
         enviarRayo(x, y, -2, -1)
     elif angulo == 110:
         enviarRayo(x, y, 2, -2)
-    elif angulo == -110:
+    elif angulo == 250:
         enviarRayo(x, y, -2, -2)
     elif angulo == 120:
         enviarRayo(x, y, 2, -3)
-    elif angulo == -120:
+    elif angulo == 240:
         enviarRayo(x, y, -2, -3)
     elif angulo == 130:
         enviarRayo(x, y, 2, -4)
-    elif angulo == -130:
+    elif angulo == 230:
         enviarRayo(x, y, -2, -4)
     elif angulo == 140:
         enviarRayo(x, y, 2, -4)
-    elif angulo == -140:
+    elif angulo == 220:
         enviarRayo(x, y, -2, -4)
     elif angulo == 150:
         enviarRayo(x, y, 2, -5)
-    elif angulo == -150:
+    elif angulo == 210:
         enviarRayo(x, y, -2, -5)
     elif angulo == 160:
         enviarRayo(x, y, 3, -6)
-    elif angulo == -160:
+    elif angulo == 200:
         enviarRayo(x, y, -3, -6)
     elif angulo == 170:
         enviarRayo(x, y, 3, -7)
-    elif angulo == -170:
+    elif angulo == 190:
         enviarRayo(x, y, -3, -7)
     elif angulo == 180:
-        enviarRayo(x, y, 3, -8)
-    elif angulo == -180:
-        enviarRayo(x, y, -3, -8)
+        enviarRayo(x, y, 0, -1)
+    # elif angulo == -180:
+    #     enviarRayo(x, y, -3, -8)
 
 
 def enviarRayo(x,y,contx,conty):
@@ -210,12 +234,17 @@ while not done:
                         calcularRayo(xSonar,ySonar,angulo)
                 if event.type == pygame.KEYUP:
                     angulo = angulo
-                print("Angulo: ", angulo)
+                # print("Angulo: ", angulo)
 
 
+        if angulo == -10:
+            angulo = 350
 
-        if angulo == -360 or angulo == 360:
+        if angulo == 360:
             angulo = 0
+
+        # if angulo == -360 or angulo == 360:
+        #     angulo = 0
         screen.fill((150, 150, 150))
         screen.blit(texto1,textRect1)
         screen.blit(texto2,textRect2)
